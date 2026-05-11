43cca1dbaaf01fcb9f4b79cc8bcb9a76fefc1ee0
Yevheniia <sopli15011970@gmail.com>
2026-05-11 20:27:59 +0200

    ToDo App

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app) 
 
 tasks = []
 
 @app.route('/')
 def home():
-    return render_template('index.html', tasks=tasks)
+    return render_template('index.html', tasks=tasks) 
 
 @app.route('/add', methods=['POST'])
 def add_task():
@@ -15,10 +18,10 @@ def add_task():
         tasks.append(task)
     return redirect('/')
 
-@app.route('/delete/<int:index>')
-def delete_task(index):
-    if 0 <= index < len(tasks):
-        tasks.pop(index)
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    if 0 <= task_id < len(tasks):
+        tasks.pop(task_id)
     return redirect('/')
 
 if __name__ == '__main__':
